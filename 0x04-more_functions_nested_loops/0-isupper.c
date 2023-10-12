65c7556d1993a90180539c9f08e016edbcc3be00
hanae564 <medasila076@gmail.com>
2023-10-12 22:03:28 +0800

    loop tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  * 0-isupper - check if the character c is upper
+ *   * @c: The character to check
+ *    * Return: On success 1.
+ *     */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
